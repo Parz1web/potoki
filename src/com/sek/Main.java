f6a403d4e038bedaaa8dbdac31738d5890a385f5
user <aasdas@asdasf.com>
2022-01-19 11:19:28 +0300

    Найти количество 2-х значных чисел

--- src/com/sek/Main.java
+++ src/com/sek/Main.java
@@ -3,38 +3,58 @@ package com.sek;
 import java.io.*;
 import java.util.Scanner;
 
+
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
         //Найти кол-во 2значных
+        //com.sek.People name age
 
-        File file = new File("test.txt");
+        People[] peoples = new People[3];
+        peoples[0] = new People("Томас",12);
+        peoples[1] = new People("Бегемот", 15);
+        peoples[2] = new People("Филипп",17);
 
-        if(file.exists())
-        {
-            System.out.println("exist");}
-        else
-        {
-            System.out.println("not");
+
+        File file = new File("peoples.txt");
+
+        FileWriter fileWriter = new FileWriter(file);
+
+
+
+        for (People str: peoples){
+            fileWriter.write(str.toString()+"\n");
         }
+
+        fileWriter.flush();
+        fileWriter.close();
+
+
+
+
 
 
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
         String str;
+
+        People[] peoplesEnd = new People[3];
+        int i = 0;
         int sum = 0;
 
-        while((str = bufferedReader.readLine()) != null)
-        {
-            if(str.length() == 2){
-                sum++;
-            }
-            else{
-                continue;
-            }
-        }
+             while((str = bufferedReader.readLine()) != null)
+             {
+                 String[] mass = str.split(",");
 
-        System.out.println(sum);
+                 peoplesEnd[i] = new People(mass[0], Integer.parseInt(mass[1]));
+                 i++;
+                 sum += Integer.parseInt(mass[1]);
+             }
+
+        System.out.println(sum/ peoplesEnd.length);
+
+
     }
 }
